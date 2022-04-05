5ffbe4f85dd3d61a12c4278a436a9a27d7f9148e
VincentHChoy <vhchoy@ualberta.ca>
2022-04-05 13:59:48 -0600

    completed survey.js

--- survey.js
+++ survey.js
@@ -5,8 +5,37 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const question1 = 'What is your name? Nicknames are also acceptable.'
+const question2 = 'What is an activity you like doing?'
+const question3 = 'What do you listen to while doing that?'
+const question4 = 'Which meal is your favourite (eg: dinner, brunch, etc.)'
+const question5 = 'What is your favourite thing to eat for that meal?'
+const question6 = 'Which sport is your absolute favourite?'
+const question7 = 'What is your superpower? In a few words, tell us what you are amazing at!'
 
-  rl.close();
+const answers = {};
+
+rl.question(question1, (answer) => {
+  answers.name = answer
+  rl.question(question2, (answer) => {
+    answers.activity = answer
+    rl.question(question3, (answer) => {
+      answers.listenWhileWorking = answer
+      rl.question(question4, (answer) => {
+        answers.favoriteMeal = answer
+        rl.question(question5, (answer) => {
+          answers.favoriteFood = answer
+          rl.question(question6, (answer) => {
+            answers.favoriteSport = answer
+            rl.question(question7, (answer) => {
+              answers.superPower = answer
+              console.log(answers)
+              console.log(`Your name is "${answers.name}" and your favorite activity is "${answers.activity}". You like to listen to $"{answers.listenWhileWorking}" while doing that. Your favorite meal is "${answers.favoriteMeal}" and your favorite food is ${answers.favoriteFood}. You're a sporty kinda person so your favorite sport is "${answers.favoriteSport}". Finally your favorite superpower is "${answers.superPower}"`)
+              rl.close()
+            });
+        });
+      });
+    });
+  });
+});
 });
